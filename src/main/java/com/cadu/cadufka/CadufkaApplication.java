dcacfc5e19951d197243cd622a571c8046f0b993
Carlos Eduardo <carlospereira@pactosolucoes.com.br>
2022-12-08 20:49:29 -0300

    first commit

---
+++ src/main/java/com/cadu/cadufka/CadufkaApplication.java
@@ -0,0 +1,13 @@
+package com.cadu.cadufka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadufkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadufkaApplication.class, args);
+	}
+
+}
