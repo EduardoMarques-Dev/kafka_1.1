1cec18f1a20cd3e68f249b7e0be2b6a788ada74c
Carlos Eduardo <carlospereira@pactosolucoes.com.br>
2022-12-10 20:15:27 -0300

    feat: 02.02 Vários consumidores e produtores

--- src/main/java/com/cadu/cadufka/domain/EmailService.java
+++ src/main/java/com/cadu/cadufka/domain/EmailService.java
@@ -2,25 +2,21 @@ package com.cadu.cadufka.domain;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.time.Duration;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 
-public class FraudDetectorService {
+public class EmailService {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         // Responsável por receber a mensagem.
         var consumer = new KafkaConsumer<String, String>(properties());
 
         // Informo o tópico que quero escutar
-        consumer.subscribe(Collections.singleton("ECOMMERCE_NEW_ORDER"));
+        consumer.subscribe(Collections.singleton("ECOMMERCE_SEND_EMAIL"));
+
 
         // Cria laço infinito para escuta contínua
         while (true){
@@ -30,18 +26,18 @@ public class FraudDetectorService {
                 System.out.println("Encontrei "+ records.count() +" registros");
                 for (var record : records){
                     System.out.println("-----------------------------------------");
-                    System.out.println("Processing new order, checking for fraud");
+                    System.out.println("Send email");
                     System.out.println("Chave: "+record.key());
                     System.out.println("Valor: "+record.value());
                     System.out.println("Partição: "+record.partition());
                     System.out.println("Offset: "+record.offset());
                     try{
-                        Thread.sleep(5000);
+                        Thread.sleep(1000);
                     }catch (InterruptedException ex){
                         //ignoring
                         ex.printStackTrace();
                     }
-                    System.out.println("Order processed");
+                    System.out.println("Email sent");
                 }
             }
 
@@ -56,7 +52,7 @@ public class FraudDetectorService {
         // Transformadores/Deserializadores de Strings para Bytes
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, FraudDetectorService.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, EmailService.class.getName());
 
         return properties;
     }
--- src/main/java/com/cadu/cadufka/domain/NewOrderMain.java
+++ src/main/java/com/cadu/cadufka/domain/NewOrderMain.java
@@ -1,5 +1,6 @@
 package com.cadu.cadufka.domain;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -12,17 +13,24 @@ public class NewOrderMain {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         // Responsável por enviar a mensagem.
         var producer = new KafkaProducer<String, String>(properties());
-        var value = "id_Pedido,id_Usuario,id_Compra";
+
         // A mensagem é um "record", pq é um registro q ficará armazenado no KAFKA
+        var value = "id_Pedido,id_Usuario,id_Compra";
         var record = new ProducerRecord<String, String>("ECOMMERCE_NEW_ORDER", value, value);
 
-        producer.send(record, (data, ex) -> {
-            if (ex != null){
+        var email = "Thank you for your order! We are processing your order!";
+        var emailRecord = new ProducerRecord<String, String>("ECOMMERCE_SEND_EMAIL",email,email);
+
+        Callback callback = (data, ex) -> {
+            if (ex != null) {
                 ex.printStackTrace();
                 return;
             }
-            System.out.println("sucesso enviado - "+data.topic() + ":::partition " + data.partition() + "/ offset " + data.offset() + "/ timestamp " + data.timestamp());
-        }).get();
+            System.out.println("sucesso enviado - " + data.topic() + ":::partition " + data.partition() + "/ offset " + data.offset() + "/ timestamp " + data.timestamp());
+        };
+
+        producer.send(record, callback).get();
+        producer.send(emailRecord, callback).get();
     }
 
     private static Properties properties(){
